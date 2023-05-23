ef6a2579668982a9f61bf46baddcecd14a7a00b3
Mudassar Hussain <mudassarhus667788@gmail.com>
2023-05-24 02:42:27 +0500

    update code

--- src/Pages/ShopForm/ShopForm.jsx
+++ src/Pages/ShopForm/ShopForm.jsx
@@ -13,16 +13,23 @@ const ShopForm = () => {
           <input type="text" id="Shop__number" placeholder='#1234' className='form_input' /><br />
           <Box mt={1}>
             <label for="Shop__honor" style={{ fontWeight: "600" }}>Shop Honor <span className="required" style={{ color: 'red', fontSize: '0.8em' }}>*</span></label>
-            <input type="text" id="Shop__honor" placeholder='#1234' className='form_input' /><br />
+            <input type="text" id="Shop__honor" placeholder='Honor Name' className='form_input' /><br />
           </Box>
-
+          <Box mt={1}>
+            <label for="starting__date" style={{ fontWeight: "600" }}>Registration Date <span className="required" style={{ color: 'red', fontSize: '0.8em' }}>*</span></label>
+            <input type="date" id="starting__date" placeholder='Honor Name' className='form_input' /><br />
+          </Box>
         </Grid>
         <Grid item xs={6} sx={{ pr: { xs: 0, md: 2 } }}>
           <label for="shop__size" style={{ fontWeight: "600", }}>Shop Size <span className="required" style={{ color: 'red', fontSize: '0.8em' }}>*</span></label>
           <input type="text" id="shop__size" placeholder='50x123' className='form_input' /><br />
           <Box mt={1}>
-            <label for="shop__rental" style={{ fontWeight: "600" }}>Shop Number <span className="required" style={{ color: 'red', fontSize: '0.8em' }}>*</span></label>
-            <input type="text" id="shop__rental" placeholder='#1234' className='form_input' /><br />
+            <label for="shop__rental" style={{ fontWeight: "600" }}>Shop Rental <span className="required" style={{ color: 'red', fontSize: '0.8em' }}>*</span></label>
+            <input type="text" id="shop__rental" placeholder='Rental Name' className='form_input' /><br />
+          </Box>
+          <Box mt={1}>
+            <label for="floor" style={{ fontWeight: "600" }}>Floor No.<span className="required" style={{ color: 'red', fontSize: '0.8em' }}>*</span></label>
+            <input type="text" id="floor" placeholder='Rental Name' className='form_input' /><br />
           </Box>
         </Grid>
       </Grid>
