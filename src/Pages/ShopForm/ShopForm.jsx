536a1d88ab0466fe3caba317f6218e379438a8f9
Mudassar Hussain <mudassarhus667788@gmail.com>
2023-05-24 03:28:32 +0500

    v

--- src/Pages/ShopForm/ShopForm.jsx
+++ src/Pages/ShopForm/ShopForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Typography, Grid } from '@mui/material'
+import { Box, Typography, Grid, Card, CardContent, CardActions, Button } from '@mui/material'
 
 
 const ShopForm = () => {
@@ -31,9 +31,25 @@ const ShopForm = () => {
             <label for="floor" style={{ fontWeight: "600" }}>Floor No.<span className="required" style={{ color: 'red', fontSize: '0.8em' }}>*</span></label>
             <input type="text" id="floor" placeholder='Rental Name' className='form_input' /><br />
           </Box>
+          {/* <Box mt={1} display='flex'>
+            <Card variant="outlined" sx={{ width: '50%' }}>
+              <CardContent>
+                <Typography variant="h6" color="initial" textAlign='center'>Remaing Rent</Typography>
+                <Typography variant="h6" color="initial" textAlign='center'>0</Typography>
+              </CardContent>
+            </Card>
+            <Card variant="outlined" sx={{ width: '50%' }}>
+              <CardContent>
+                <Typography variant="h6" color="initial" textAlign='center'>Remaing Rent</Typography>
+                <Typography variant="h6" color="initial" textAlign='center'>20,000</Typography>
+              </CardContent>
+            </Card>
+          </Box> */}
         </Grid>
       </Grid>
-
+      <Box mt={2} pr={2}>
+        <Button sx={{width:"100%"}}> Add New</Button>
+      </Box>
     </Box>
   )
 }
