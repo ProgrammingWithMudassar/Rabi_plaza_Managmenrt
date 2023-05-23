d788c9aa54fdd3811405e7d4999776e199b89240
Mudassar Hussain <mudassarhus667788@gmail.com>
2023-05-24 02:00:06 +0500

    update code

--- src/Pages/ShopForm/ShopForm.jsx
+++ src/Pages/ShopForm/ShopForm.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Box, Typography, Grid } from '@mui/material'
+
 
 const ShopForm = () => {
   return (
-    <div>
-        this is shop form
-    </div>
+    <Box>
+      <Typography variant="h5" color="initial" my={2}>Add New Shop</Typography>
+
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <label for="ShopNo" style={{ fontWeight: "600", }}>Shop Number</label>
+          <input type="text" id="ShopNo" placeholder='Shop No' className='form_input' /><br />
+        </Grid>
+        <Grid item xs={6} sx={{ pr: { xs: 0, md: 2 } }}>
+          <label for="ShopNo" style={{ fontWeight: "600", }}>Shop Number</label>
+          <input type="text" id="ShopNo" placeholder='Shop No' className='form_input' /><br />
+        </Grid>
+      </Grid>
+
+    </Box>
   )
 }
 
