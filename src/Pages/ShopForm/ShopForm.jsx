8a7bebfd764c1c9c90a4a0b1bc59e2baa66896f3
Mudassar Hussain <mudassarhus667788@gmail.com>
2023-05-24 02:31:52 +0500

    update code

--- src/Pages/ShopForm/ShopForm.jsx
+++ src/Pages/ShopForm/ShopForm.jsx
@@ -9,12 +9,21 @@ const ShopForm = () => {
 
       <Grid container spacing={2}>
         <Grid item xs={6}>
-          <label for="ShopNo" style={{ fontWeight: "600", }}>Shop Number</label>
-          <input type="text" id="ShopNo" placeholder='Shop No' className='form_input' /><br />
+          <label for="Shop__number" style={{ fontWeight: "600", }}>Shop Number <span className="required" style={{ color: 'red', fontSize: '0.8em' }}>*</span></label>
+          <input type="text" id="Shop__number" placeholder='#1234' className='form_input' /><br />
+          <Box mt={1}>
+            <label for="Shop__honor" style={{ fontWeight: "600" }}>Shop Honor <span className="required" style={{ color: 'red', fontSize: '0.8em' }}>*</span></label>
+            <input type="text" id="Shop__honor" placeholder='#1234' className='form_input' /><br />
+          </Box>
+
         </Grid>
         <Grid item xs={6} sx={{ pr: { xs: 0, md: 2 } }}>
-          <label for="ShopNo" style={{ fontWeight: "600", }}>Shop Number</label>
-          <input type="text" id="ShopNo" placeholder='Shop No' className='form_input' /><br />
+          <label for="shop__size" style={{ fontWeight: "600", }}>Shop Size <span className="required" style={{ color: 'red', fontSize: '0.8em' }}>*</span></label>
+          <input type="text" id="shop__size" placeholder='50x123' className='form_input' /><br />
+          <Box mt={1}>
+            <label for="shop__rental" style={{ fontWeight: "600" }}>Shop Number <span className="required" style={{ color: 'red', fontSize: '0.8em' }}>*</span></label>
+            <input type="text" id="shop__rental" placeholder='#1234' className='form_input' /><br />
+          </Box>
         </Grid>
       </Grid>
 
