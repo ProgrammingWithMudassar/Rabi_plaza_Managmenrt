c705c64b52a459ae9f25d72c9b28ec70db2f6b05
Mudassar Hussain <mudassarhus667788@gmail.com>
2023-05-25 03:18:58 +0500

    update code

--- src/Data/DummyData.js
+++ src/Data/DummyData.js
@@ -64,6 +64,15 @@ export const HomeCardData = [
     },
 ]
 
-export const ShopData = [
-    
-]
+export const  rows = [
+    { id: 1, Shop_No: 1, rental: 'Snow', S_honor: 'Jon', size: 35, floor: 2, S_date: "12/3/2023", r_rent: 2500 },
+    { id: 2, Shop_No: 2, rental: 'Lannister', S_honor: 'Cersei', size: 42, floor: 2, S_date: "12/3/2023", r_rent: 0 },
+    { id: 3, Shop_No: 3, rental: 'Lannister', S_honor: 'Jaime', size: 45, floor: 2, S_date: "12/3/2023", r_rent: 2000 },
+    { id: 4, Shop_No: 4, rental: 'Stark', S_honor: 'Arya', size: 16, floor: 2, S_date: "12/3/2023", r_rent: 3000 },
+    { id: 5, Shop_No: 5, rental: 'Targaryen', S_honor: 'Daenerys', size: 34, floor: 2, S_date: "12/3/2023", r_rent: 2000 },
+    { id: 6, Shop_No: 6, rental: 'Melisandre', S_honor: null, size: 15, floor: 20, S_date: "12/3/2023", r_rent: 2000 },
+    { id: 7, Shop_No: 7, rental: 'Clifford', S_honor: 'Ferrara', size: 44, floor: 2, S_date: "12/3/2023", r_rent: 2000 },
+    { id: 8, Shop_No: 8, rental: 'Frances', S_honor: 'Rossini', size: 36, floor: 2, S_date: "12/3/2023", r_rent: 2000 },
+    { id: 9, Shop_No: 9, rental: 'Roxie', S_honor: 'Harvey', size: 65, floor: 2, S_date: "12/3/2023", r_rent: 2000 },
+  ];
+  
--- src/Pages/Shop_Details/Shop_Details.jsx
+++ src/Pages/Shop_Details/Shop_Details.jsx
@@ -1,19 +1,43 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
-import {Typography,Box} from '@mui/material'
+import { Button, Box, Typography } from '@mui/material'
+import { rows } from '../../Data/DummyData.js';
+import { useHistory } from 'react-router-dom';
 
 const Shop_Details = () => {
-    const { id } = useParams();
 
-    // Logic to fetch the details of the shop with the given ID
-  
-    return (
-      <Box>
-        <h2>Shop Details - ID: {id}</h2>
-        <Typography variant="body1" color="initial">dsfgs</Typography>
-        {/* Display the details of the shop */}
-      </Box>
-    );
+  const history = useHistory();
+  const { id } = useParams();
+  const shop = rows.find((row) => row.id === Number(id));
+
+  const goBack = () => {
+    window.history.go(-1);
+  };
+
+  if (!shop) {
+    return <div>
+      <Typography variant="h5" color="initial" textAlign='center'>Shop not found</Typography>
+    </div>;
+  }
+
+
+  return (
+    <Box>
+        <Button onClick={goBack}>Go Back</Button>
+      <h2>Shop Details - ID: {id}</h2>
+      {shop && (
+        <div>
+          <p>Shop Number: {shop.Shop_No}</p>
+          <p>Rental: {shop.rental}</p>
+          <p>Shop Honor: {shop.S_honor}</p>
+          <p>Shop Size: {shop.size}</p>
+          <p>Floor: {shop.floor}</p>
+          <p>Starting Date: {shop.S_date}</p>
+          <p>Remaining Rent: {shop.r_rent}</p>
+        </div>
+      )}
+    </Box>
+  );
 }
 
 export default Shop_Details
