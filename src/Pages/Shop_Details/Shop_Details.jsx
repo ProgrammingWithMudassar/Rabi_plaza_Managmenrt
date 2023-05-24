be85cf4ae2cfd13e5d1ca0903e1dd87d9d33f180
Mudassar Hussain <mudassarhus667788@gmail.com>
2023-05-25 03:56:25 +0500

    update code

--- src/Pages/Shop_Details/Shop_Details.jsx
+++ src/Pages/Shop_Details/Shop_Details.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
-import { Button, Box, Typography } from '@mui/material'
+import { Button, Box, Typography, Card, CardContent, CardActions } from '@mui/material'
 import { rows } from '../../Data/DummyData.js';
-import { useHistory } from 'react-router-dom';
+import { faBackward  } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 const Shop_Details = () => {
 
-  const history = useHistory();
   const { id } = useParams();
   const shop = rows.find((row) => row.id === Number(id));
 
@@ -23,18 +24,27 @@ const Shop_Details = () => {
 
   return (
     <Box>
-        <Button onClick={goBack}>Go Back</Button>
-      <h2>Shop Details - ID: {id}</h2>
       {shop && (
-        <div>
-          <p>Shop Number: {shop.Shop_No}</p>
+        <Box mr={2} >
+          <Box display='flex' justifyContent='space-between' py={2}>
+            <h2>Shop Rental:<span style={{color:"#FF8E53"}}> {shop.rental}</span></h2>
+            <Button onClick={goBack}>  <FontAwesomeIcon icon={faBackward} /> <span style={{marginLeft:"7px"}}> Go Back</span></Button>
+          </Box>
+
+          <Card variant="outlined" >
+            <CardContent>
+              
+            </CardContent>
+
+          </Card>
+          {/* <p>Shop Number: {shop.Shop_No}</p>
           <p>Rental: {shop.rental}</p>
           <p>Shop Honor: {shop.S_honor}</p>
           <p>Shop Size: {shop.size}</p>
           <p>Floor: {shop.floor}</p>
           <p>Starting Date: {shop.S_date}</p>
-          <p>Remaining Rent: {shop.r_rent}</p>
-        </div>
+          <p>Remaining Rent: {shop.r_rent}</p> */}
+        </Box>
       )}
     </Box>
   );
