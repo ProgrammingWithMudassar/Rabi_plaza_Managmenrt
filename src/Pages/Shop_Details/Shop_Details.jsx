a0242265284850c0ec7a069626f7bfed463589cf
Mudassar Hussain <mudassarhus667788@gmail.com>
2023-05-25 03:59:37 +0500

    update code

--- src/Pages/Shop_Details/Shop_Details.jsx
+++ src/Pages/Shop_Details/Shop_Details.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
-import { Button, Box, Typography, Card, CardContent, CardActions } from '@mui/material'
+import { Button, Box, Typography,Grid, Card, ListItem, List  } from '@mui/material'
 import { rows } from '../../Data/DummyData.js';
-import { faBackward  } from '@fortawesome/free-solid-svg-icons';
+import { faBackward } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
@@ -27,15 +27,32 @@ const Shop_Details = () => {
       {shop && (
         <Box mr={2} >
           <Box display='flex' justifyContent='space-between' py={2}>
-            <h2>Shop Rental:<span style={{color:"#FF8E53"}}> {shop.rental}</span></h2>
-            <Button onClick={goBack}>  <FontAwesomeIcon icon={faBackward} /> <span style={{marginLeft:"7px"}}> Go Back</span></Button>
+            <h2>Shop Rental:<span style={{ color: "#FF8E53" }}> {shop.rental}</span></h2>
+            <Button onClick={goBack}>  <FontAwesomeIcon icon={faBackward} /> <span style={{ marginLeft: "7px" }}> Go Back</span></Button>
           </Box>
 
-          <Card variant="outlined" >
-            <CardContent>
-              
-            </CardContent>
-
+          <Card >
+            <List>
+              <Grid container spacing={0}>
+                <Grid xs={3} md={3}>
+                  <ListItem><Typography variant="body1" fontWeight={600}>Age :</Typography></ListItem>
+                  <ListItem><Typography variant="body1" fontWeight={600}>Email:</Typography></ListItem>
+                  <ListItem><Typography variant="body1" fontWeight={600}>Number:</Typography></ListItem>
+                  <ListItem><Typography variant="body1" fontWeight={600}>Degree:</Typography></ListItem>
+                  <ListItem><Typography variant="body1" fontWeight={600}>Freelance:</Typography></ListItem>
+                </Grid>
+                <Grid xs={9} md={9}>
+                  <ListItem><Typography variant="body1">22</Typography></ListItem>
+                  <ListItem><Typography variant="body1">mudassar0920@gmail.com</Typography></ListItem>
+                  <ListItem><Typography variant="body1">+923104602885</Typography></ListItem>
+                  <ListItem><Typography variant="body1">BS Software Engineering</Typography></ListItem>
+                  <ListItem><Typography variant="body1">Availabel</Typography></ListItem>
+                  <ListItem>
+                    <Button sx={{ fontSize: '12px' }}>Download CV</Button>
+                  </ListItem>
+                </Grid>
+              </Grid>
+            </List>
           </Card>
           {/* <p>Shop Number: {shop.Shop_No}</p>
           <p>Rental: {shop.rental}</p>
