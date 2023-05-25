06f168c50add8a7240dbe00ee662076597709026
Mudassar Hussain <mudassarhus667788@gmail.com>
2023-05-26 04:05:30 +0500

    update code

--- src/Pages/Expence_Calculator/Expence_Calculator.jsx
+++ src/Pages/Expence_Calculator/Expence_Calculator.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Typography, Box, Grid } from '@mui/material';
+import { Button, Typography, Box, Grid, Card, CardContent, CardActions, Divider } from '@mui/material';
 
 const ExpenseCalculator = () => {
     const [inputFields, setInputFields] = useState([{ firstName: '', secondName: '' }]);
@@ -38,10 +38,10 @@ const ExpenseCalculator = () => {
                     className="e_input"
                 />
                 <input
-                    type="text"
+                    type="Number"
                     value={field.secondName}
                     onChange={(event) => handleChange(index, 'secondName', event)}
-                    placeholder="Second Name"
+                    placeholder="Amount"
                     className="e_input"
                     style={{ marginLeft: '10px' }}
                 />
@@ -56,41 +56,55 @@ const ExpenseCalculator = () => {
 
     return (
         <div>
-            <Grid container spacing={2}>
-                <Grid item xs={6}></Grid>
-                <Grid item xs={5}>
-                    <Box mt={2} display='flex' width="40%" justifyContent="space-between">
-                        <Typography variant="body1" color="initial">This Month Expence</Typography>
-                        <Typography variant="body1" color="initial" fontWeight={600}>60,000</Typography>
-                    </Box>
-                    <Box mt={2} display='flex' width="40%" justifyContent="space-between">
-                        <Typography variant="body1" color="initial">This Month Income</Typography>
-                        <Typography variant="body1" color="initial" fontWeight={600}>60,000</Typography>
-                    </Box>
-                </Grid>
-            </Grid>
             <Typography variant="h6" color="initial" my={2}>
                 Expense Calculator
             </Typography>
-            <form onSubmit={handleSubmit}>
-                {renderInputFields()}
-                <Button type="button" onClick={() => handleAddFields()} sx={{ mt: 2 }}>
-                    Add Field
-                </Button>
-                <br />
+            <Grid container spacing={2}>
+                <Grid item xs={6}>
+                    <form onSubmit={handleSubmit}>
+                        {renderInputFields()}
+                        <Button type="button" onClick={() => handleAddFields()} sx={{ mt: 2 }}>
+                            Add Field
+                        </Button>
+                        <br />
 
-                <Button type="submit" sx={{
-                    width: '200px', mt: 4,
-                    backgroundColor: '#096AFF',
-                    boxShadow: '0 3px 5px 2px rgba(9, 106, 255, .3)',
-                    ':hover': {
-                        backgroundColor: '#096AFF',
-                        border: "1px solid #096AFF"
-                    },
-                }}>
-                    Calculate Expense
-                </Button>
-            </form>
+
+                    </form>
+                </Grid>
+                <Grid item xs={5}>
+                    <Card variant="outlined">
+                        <CardContent>
+                            <Box mt={2} display='flex' justifyContent="space-between">
+                                <Typography variant="h6" fontWeight={600} color="initial">Expence</Typography>
+                                <Typography variant="body1" color="initial" fontWeight={600}>60,000</Typography>
+                            </Box>
+                            <Box mt={2} display='flex' justifyContent="space-between">
+                                <Typography variant="h6" fontWeight={600} color="initial">Income</Typography>
+                                <Typography variant="body1" color="initial" fontWeight={600}>60,000</Typography>
+                            </Box>
+                            <Divider
+                              variant="fullWidth"
+                              orientation="horizontal"
+                            />
+                        </CardContent>
+                        <CardActions>
+                            <Button type="submit" sx={{
+                                width: '100%', mt: 4,
+                                backgroundColor: '#096AFF',
+                                boxShadow: '0 3px 5px 2px rgba(9, 106, 255, .3)',
+                                ':hover': {
+                                    backgroundColor: '#096AFF',
+                                    border: "1px solid #096AFF"
+                                },
+                            }}>
+                                Calculate Expense
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+            </Grid>
+
+
         </div>
     );
 };
