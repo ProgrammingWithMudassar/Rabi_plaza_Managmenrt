e651c43a5777f927bbcd5ea8f05018236a068796
Mudassar Hussain <mudassarhus667788@gmail.com>
2023-05-26 03:40:31 +0500

    update code

--- src/Pages/Expence_Calculator/Expence_Calculator.jsx
+++ src/Pages/Expence_Calculator/Expence_Calculator.jsx
@@ -1,12 +1,76 @@
-import React from 'react'
-import { Box, Typography } from '@mui/material'
+import React, { useState } from 'react';
+import { Button, Typography } from '@mui/material';
 
-const Expence_Calculator = () => {
-    return (
-        <Box>
-            <Typography variant="h6" color="initial" my={2}>Expence Calculator</Typography>
-        </Box>
-    )
-}
+const ExpenseCalculator = () => {
+  const [inputFields, setInputFields] = useState([{ firstName: '', secondName: '' }]);
 
-export default Expence_Calculator
+  const handleChange = (index, fieldName, event) => {
+    const values = [...inputFields];
+    values[index][fieldName] = event.target.value;
+    setInputFields(values);
+  };
+
+  const handleAddFields = () => {
+    const values = [...inputFields];
+    values.push({ firstName: '', secondName: '' });
+    setInputFields(values);
+  };
+
+  const handleRemoveFields = (index) => {
+    const values = [...inputFields];
+    values.splice(index, 1);
+    setInputFields(values);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputFields); // Do something with the data
+  };
+
+  const renderInputFields = () => {
+    return inputFields.map((field, index) => (
+      <div key={index} style={{ marginTop: '10px' }}>
+        <input
+          type="text"
+          value={field.firstName}
+          onChange={(event) => handleChange(index, 'firstName', event)}
+          placeholder="Expense Name"
+          className="e_input"
+        />
+        <input
+          type="text"
+          value={field.secondName}
+          onChange={(event) => handleChange(index, 'secondName', event)}
+          placeholder="Second Name"
+          className="e_input"
+          style={{ marginLeft: '10px' }}
+        />
+        {inputFields.length > 1 && (
+          <Button type="button" style={{ marginLeft: '10px' }} onClick={() => handleRemoveFields(index)}>
+            Remove
+          </Button>
+        )}
+      </div>
+    ));
+  };
+
+  return (
+    <div>
+      <Typography variant="h6" color="initial" my={2}>
+        Expense Calculator
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        {renderInputFields()}
+        <Button type="button" onClick={() => handleAddFields()}>
+          Add Field
+        </Button>
+        <br />
+        <Button type="submit" sx={{ width: '200px', mt: 4 }}>
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default ExpenseCalculator;
