487d7c273cbd5a1f5a8c39d16f33dd0ed88bb4bd
Mudassar Hussain <mudassarhus667788@gmail.com>
2023-06-02 00:29:33 +0500

    update code

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,9 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { Box } from '@mui/material'
 import { HomeCard } from '../Components/index'
 import './Style.css'
 
 const Home = () => {
-  const card = {
-
-  }
   return (
     <Box sx={{ pr: 1, width: '100%',overflow:'hidden' }} >
       <HomeCard />
@@ -15,24 +12,3 @@ const Home = () => {
 }
 
 export default Home
-
-
-
-// <Grid container spacing={2}>
-//         <Grid item xs={12} sm={4} md={12} sx={{ display: 'flex', width: '100%' }}>
-//           {
-//             CardData.map((data, index) => {
-//               return (
-//                 <Card variant="outlined">
-//                   <CardContent>
-//                     <data.icon />
-//                   </CardContent>
-//                   <CardActions>
-//                     <Button>Learn more</Button>
-//                   </CardActions>
-//                 </Card>
-//               )
-//             })
-//           }
-//         </Grid>
-//       </Grid>
